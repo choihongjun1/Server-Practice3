910bd4c375d314624be6d079c07128930b730450
choihongjun <vvclsbalam@gmail.com>
2025-04-01 15:08:56 +0900

    feat: 롬복 테스트

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,9 @@
+package jpabook.jpashop;
+
+public class HelloController {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.setName("test");
+        System.out.println(hello.getName());
+    }
+}
