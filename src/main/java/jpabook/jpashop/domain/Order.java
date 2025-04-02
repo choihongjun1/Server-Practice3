477db02e23309d02ef24deade3107de882dd80fe
choihongjun <vvclsbalam@gmail.com>
2025-04-03 03:15:07 +0900

    feat: Order 엔티티 구현

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders") // 테이블 이름 지정
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY) // 다대일,
+    @JoinColumn(name = "member_id") // fk가 member_id
+    private Member member; // 주문 회원
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 일대다,
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY) // 일대일
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery; // 배송정보
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING) // enum의 이름을 문자열로 저장
+    private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
