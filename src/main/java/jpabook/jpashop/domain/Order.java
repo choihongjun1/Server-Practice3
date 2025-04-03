a7946ccb1177cfee7ae94124aa8f403c11b1993d
choihongjun <vvclsbalam@gmail.com>
2025-04-03 12:34:26 +0900

    feat: OrderItem 엔티티 구현

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -13,19 +13,19 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY) // 다대일 ??
+    @ManyToOne(fetch = FetchType.LAZY) // 다대일 지연로딩
     @JoinColumn(name = "member_id") // member_id가 fk
     private Member member; // 주문 회원
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 일대다,
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 일대다,OrderItem의 order가 관계 주인, OrderItem이 Order를 참조, Order의 상태 변화가 OrderItem에도 전파
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY) // 일대일
-    @JoinColumn(name = "delivery_id")
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY) // 일대일, 지연로딩 엔티티가 실제로 필요할 때 로딩
+    @JoinColumn(name = "delivery_id") // delivery_id가 fk
     private Delivery delivery; // 배송정보
     private LocalDateTime orderDate; // 주문시간
 
-    @Enumerated(EnumType.STRING) // enum의 이름을 문자열로 저장
+    @Enumerated(EnumType.STRING) // 속성이 enum 질때 DB에 저장할 방법 지정 (enum 문자열 이름)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
     public void setMember(Member member) {
