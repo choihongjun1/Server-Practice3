d2514a852dff532b3953de1474f792bec51cb15b
choihongjun <vvclsbalam@gmail.com>
2025-04-05 16:38:44 +0900

    fix: NullPointerException 처리

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,12 +6,14 @@ import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
+// @Rollback(false)
 public class MemberServiceTest {
 
     @Autowired MemberService memberService;
