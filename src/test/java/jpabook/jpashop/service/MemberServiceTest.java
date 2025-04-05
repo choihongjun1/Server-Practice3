a576319f25c39022f749511120c50987d30b92af
choihongjun <vvclsbalam@gmail.com>
2025-04-05 16:17:11 +0900

    feat: 회원가입 테스트

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.service;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // Given
+        Member member = new Member();
+        member.setName("kim");
+
+        // When
+        Long saveId = memberService.join(member);
+
+        // Then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+}
