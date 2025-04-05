ac99bc8e7e14dac846c51c28279b89dd96c3e6c4
choihongjun <vvclsbalam@gmail.com>
2025-04-05 16:56:19 +0900

    fix: 중복회원가입 테스트

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -32,5 +32,27 @@ public class MemberServiceTest {
         // Then
         em.flush();
         assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // Given
+        Member member1 = new Member();
+        member1.setName("kim");
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // When
+        memberService.join(member1);
+//        memberService.join(member1);
+//        try {
+//            memberService.join(member2);
+//        } catch (IllegalStateException e) {
+//            return;
+//        }
+
+        // Then
+        //IllegalStateException 예외가 발생하지 않으면 테스트 실패
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
     }
 }
