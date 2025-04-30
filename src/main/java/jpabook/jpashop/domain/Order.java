f6e26220ed867d7e0a1acd79c556ae6f67b91644
choihongjun <vvclsbalam@gmail.com>
2025-04-30 21:14:14 +0900

    feat: Order 및 OrderItem 클래스 protected 생성자 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
@@ -11,6 +13,7 @@ import java.util.List;
 @Entity
 @Table(name = "orders") // 테이블 이름 지정
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // protected 기본 생성자
 public class Order {
 
     @Id @GeneratedValue
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,13 +2,16 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "order_item")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // protected 기본 생성자
 public class OrderItem {
 
     @Id @GeneratedValue
