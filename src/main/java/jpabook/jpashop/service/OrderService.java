410cb10777b3d7a4dfbfa392f5c940838e9876a9
choihongjun <vvclsbalam@gmail.com>
2025-05-01 16:08:30 +0900

    feat: findOrders 메소드 구현

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -50,9 +52,9 @@ public class OrderService {
         order.cancel();
     }
 
-//    /** 주문 검색 */
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    /** 주문 검색 */
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAll(orderSearch);
+    }
 
 }
